f7a3cb14461b1940ec46c2a40db7be6b3891e2bd
Lucía <14921905-lmaio@users.noreply.gitlab.com>
2024-04-23 15:59:43 +0200

    feat: seo and javascript refactor

---
+++ docs/js/docsify-config.js
@@ -0,0 +1,15 @@
+const repository = "https://github.com/trans-en-espanol/transespdiscord-landing";
+
+window.$docsify = {
+    name: "Trans en Español",
+    repo: repository,
+    coverpage: true,
+    onlyCover: true,
+    loadSidebar: true,
+    autoHeader: true,
+    subMaxLevel: 2,
+    search: {
+        placeholder: "Escribe aquí para buscar",
+        noData: "Sin resultados."
+    }
+}
